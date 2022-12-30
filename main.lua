ff93e6f42f8a3659d0926e633b97d9bc2b102eca
Kevin.Tung <chen0625tung@gmail.com>
2022-12-31 02:28:09 +0800

    + modified mouseTouched

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local rows, cols, grid
-local cell_width = 10
+local cell_width = 5
 local counter = 0
 local freq = 0.05
 
@@ -17,21 +17,34 @@ local function make2DArray(r, c)
 	return arr
 end
 
-local function fill2DArray(arr, r)
+local function fill2DArray(arr)
 	math.randomseed(os.time())
 
 	for _, inner_arr in ipairs(arr) do
-		for i = 1, r do
+		for i = 1, #inner_arr do
 			inner_arr[i] = math.random(0, 1)
 		end
 	end
 end
 
 local function checkNeighbor(g, x, y)
-	if x >= 1 and x <= rows and y >= 1 and y <= cols then
+	--[[ if x >= 1 and x <= rows and y >= 1 and y <= cols then
 		return g[x][y] == 1
 	end
-	return false
+	return false ]]
+	if x > rows then
+		x = x - rows
+	end
+	if x < 1 then
+		x = x + rows
+	end
+	if y > cols then
+		y = y - cols
+	end
+	if y < 1 then
+		y = y + cols
+	end
+	return g[x][y] == 1
 end
 
 function love.load()
@@ -40,7 +53,7 @@ function love.load()
 	cols = love.graphics.getHeight() / cell_width
 
 	grid = make2DArray(rows, cols)
-	fill2DArray(grid, rows)
+	fill2DArray(grid)
 end
 
 function love.update(dt)
@@ -87,13 +100,33 @@ function love.keypressed(key)
 	if key == "escape" then
 		love.event.quit()
 	end
+
+	if key == "space" then
+		fill2DArray(grid)
+	end
+end
+
+function love.mousepressed(x, y, button, istouch)
+	if button == 1 then
+		for i = 1, cols do
+			for j = 1, rows do
+				if x >= (j - 1) * cell_width and x <= (j + 1) * cell_width then
+					if y >= (i - 1) * cell_width and y <= (i + 1) * cell_width then
+						if grid[j][i] == 0 then
+							grid[j][i] = 1
+						end
+					end
+				end
+			end
+		end
+	end
 end
 
 function love.draw()
-	love.graphics.setBackgroundColor(1, 1, 1)
+	love.graphics.setBackgroundColor(0.8, 0.8, 0.9)
 	for i = 1, cols do
 		for j = 1, rows do
-			love.graphics.setColor(0, 0, 0)
+			love.graphics.setColor(0.3, 0.3, 0.4)
 			if grid[j][i] == 1 then
 				love.graphics.rectangle("fill", (j - 1) * cell_width, (i - 1) * cell_width, cell_width, cell_width)
 			else
